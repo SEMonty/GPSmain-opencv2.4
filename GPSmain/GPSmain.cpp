fa439a7cefa4d08f3375a50cb3a4a93cc2845e27
Motohiro Shibakawa <c201075@gmail.com>
2017-06-08 19:45:02 +0900

    OpenCV2.4.11でのサンプルプロジェクト

--- GPSmain/GPSmain.cpp
+++ GPSmain/GPSmain.cpp
@@ -3,9 +3,25 @@
 
 #include "stdafx.h"
 
+#include <opencv2/nonfree/nonfree.hpp>
+#include <opencv/highgui.h>
 
-int _tmain(int argc, _TCHAR* argv[])
+void main()
 {
-	return 0;
-}
+	// 画像読み込み
+	const cv::Mat input = cv::imread("C:\\temp\\p.jpg", 1);
 
+	// SIFTによる特徴検出
+	cv::SiftFeatureDetector detector;
+	std::vector<cv::KeyPoint> keypoints;
+	detector.detect(input, keypoints);
+
+	// キーポイントの書き込み
+	cv::Mat output;
+	cv::drawKeypoints(input, keypoints, output);
+
+	// キーポイントを書き込んだ画像の表示
+	cv::imshow("SIFT", output);
+
+	cv::waitKey();
+}
